2b251de566dc6a7bf629553e3a8cdb9c65990563
ivanf-09 <ivanfan09@gmail.com>
2022-11-29 15:48:25 +0800

    added gameover condition

--- main.js
+++ main.js
@@ -2,13 +2,22 @@ $(document).ready(function () {
     const cv = $("canvas").get(0);
     const context = cv.getContext("2d");
 
-    const sounds = {};
+    const sounds = {
+        // background: new Audio("soundtrack/background.mp3"),
+        // gameover: new Audio("soundtrack/gameover.mp3"),
+        explosion: new Audio("soundtrack/explosion.mp3"),
+        enemyhit: new Audio("soundtrack/enemyhit.mp3"),
+        getshield: new Audio("soundtrack/getshield.mp3"),
+        getboot: new Audio("soundtrack/getboot.mp3"),
+        getheart: new Audio("soundtrack/getheart.mp3"),
+    
+    };
 
     const totalgametime = 180;
     let gamestarttime = 0;
 
     //boundary of gamearea, should be const, using let for temp
-    const top = 180;
+    const top = 100;
     const left = 50;
     const bottom = 700;
     const right = 1450;
@@ -59,32 +68,22 @@ $(document).ready(function () {
     const player1 = Player(context, left, bottom, gamearea);
     const player2 = Player(context, right, bottom, gamearea);
 
-    //Create health bar
-    // const healthBarWidth = 200;
-    // const healthBarHeight = 30;
-    // const x = cv.width / 2 - healthBarWidth / 2;
-    // const y = cv.height / 2 - healthBarHeight / 2;
-
-    // const healthBar = new HealthBar(70, 45, 100, 30, 100, "green");
-
     //Create items
-    const maxbomb = 12, maxenemy = 6, maxheart = 3, maxspitem = 2;
+    const maxbomb = 20, maxenemy = 6, maxheart = 3, maxspitem = 2;
     const bombs = [], enemies = [], hearts = [], shields = [], boots = [];
     
     const enemy_y_range = [200, 200, 400, 400, 600, 600];
-    const bomb_y_range = [200, 200, 200, 400, 400, 400, 600, 600, 600, 730, 730, 730];
-    const heart_y_range = [200, 400, 600, 730]
-    const spitem_y_range = [200, 400, 600, 730];
+    const bomb_y_range = [200, 200, 200, 200, 200, 400, 400, 400, 400, 400, 600, 600, 600, 600, 600, 700, 700, 700, 700, 700];
+    const heart_y_range = [200, 400, 600, 700];
+    const spitem_y_range = [200, 400, 600, 700];
 
-    for (let i=0; i<maxbomb; i++) bombs.push(Bomb(context, Math.random() * (right - left) + left, -100))
-    for (let i=0; i<maxenemy; i++) enemies.push(Enemy(context, Math.random() * (right - left) + left, enemy_y_range[i] - 30));
-    for (let i=0; i<maxheart; i++) hearts.push(Heart(context, Math.random() * (right - left) + left, heart_y_range[Math.floor(Math.random() * 4)] - 30));
-    for (let i=0; i<maxspitem; i++) shields.push(Shield(context, Math.random() * (right - left) + left, spitem_y_range[Math.floor(Math.random() * 4)] - 30));
-    for (let i=0; i<maxspitem; i++) boots.push(Boot(context, Math.random() * (right - left) + left, spitem_y_range[Math.floor(Math.random() * 4)] - 30));
+    for (let i=0; i<maxbomb; i++) bombs.push(Bomb(context, Math.random() * (right - left) + left, Math.floor(Math.random() * -1000) - 100))
+    for (let i=0; i<maxenemy; i++) enemies.push(Enemy(context, Math.random() * (right - left) + left, enemy_y_range[i]));
+    for (let i=0; i<maxheart; i++) hearts.push(Heart(context, Math.random() * (right - left) + left, heart_y_range[Math.floor(Math.random() * 4)]));
+    for (let i=0; i<maxspitem; i++) shields.push(Shield(context, Math.random() * (right - left) + left, spitem_y_range[Math.floor(Math.random() * 4)]));
+    for (let i=0; i<maxspitem; i++) boots.push(Boot(context, Math.random() * (right - left) + left, spitem_y_range[Math.floor(Math.random() * 4)]));
 
     function doFrame(now) {
-
-        
 
         //Handle game start and gameover
         if (gamestarttime == 0) gamestarttime = now;
@@ -98,7 +97,16 @@ $(document).ready(function () {
         if (timeRemaining == 0) {
             //Show gameover page
             $('#game-over').show();
-            // if ()
+            return 0;
+        }
+        if ($("#p1health").val() == 0 || player2.getBoundingBox().isPointInBox(800, 80)) {
+            $('#game-over').show();
+            $("#winner").text("P2");
+            return 0;
+        }
+        if ($("#p2health").val() == 0 || player1.getBoundingBox().isPointInBox(800, 80)){
+            $('#game-over').show();
+            $("#winner").text("P1");
             return 0;
         }
         
@@ -116,21 +124,13 @@ $(document).ready(function () {
         enemies.forEach(shield => {shield.update(now);});
         boots.forEach(boot => {boot.update(now);});
 
-        //Generate health bar
-        // healthBar.show(context);
-        // csv.onclick = function() {
-        //     health -= 10;
-        //     healthBar.updateHealth(health);
-        // };
-
         //Generate object
-        // for (let i=0; i<maxbomb; i++) yarr.push(Math.random() * (bottom - top) + top);
         dropBomb(bombs, bomb_y_range, now, left, right);
-        checkTouchBomb(player1, player2, bombs, bomb_y_range, left, right)
+        checkTouchBomb(player1, player2, bombs, left, right, sounds)
         enemyMove(enemies, left, right);
-        checkTouchEnemy(player1, player2, enemies, enemy_y_range, left, right)
-        chekcTouchSPItem(player1, player2, shields, boots, spitem_y_range, left, right)
-        checkTouchHeart(player1, player2, hearts, heart_y_range, left, right)
+        checkTouchEnemy(player1, player2, enemies, enemy_y_range, left, right, sounds)
+        chekcTouchSPItem(player1, player2, shields, boots, spitem_y_range, left, right, sounds)
+        checkTouchHeart(player1, player2, hearts, heart_y_range, left, right, sounds)
 
         //Clear the screen
         context.clearRect(0, 0, cv.width, cv.height);
