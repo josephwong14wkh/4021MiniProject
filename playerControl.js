bb20694f3785d52db44ee8cc9e225f9f2521257e
ivanf-09 <ivanfan09@gmail.com>
2022-11-29 15:45:55 +0800

    renew cheat mode

--- playerControl.js
+++ playerControl.js
@@ -2,13 +2,21 @@ const moving = (player1, player2) => {
     $(document).on("keydown", function (event) {
         /* Handle the key down */
         switch (event.keyCode) {
-            case 72:player1.speedUp();break;
+            case 72:{
+                $("#p1shield").text(999);
+                player1.speedUp();
+                break;
+            }
             case 65:player1.move(1);break;
             case 87:player1.move(2);break;
             case 68:player1.move(3);break;
             case 83:player1.move(4);break;
 
-            case 222:player2.speedUp();break;
+            case 222:{
+                $("#p2shield").text(999);
+                player2.speedUp();
+                break;
+            }
             case 37:player2.move(1);break;
             case 38:player2.move(2);break;
             case 39:player2.move(3);break;
@@ -22,13 +30,11 @@ const stopping = (player1, player2) => {
     $(document).on("keyup", function (event) {
         /* Handle the key up */
         switch (event.keyCode) {
-            case 72:player1.changeSpeed(50);break;
             case 65:player1.stop(1);break;
             case 87:player1.stop(2);break;
             case 68:player1.stop(3);break;
             case 83:player1.stop(4);break;
 
-            case 222:player2.changeSpeed(50);break;
             case 37:player2.stop(1);break;
             case 38:player2.stop(2);break;
             case 39:player2.stop(3);break;
